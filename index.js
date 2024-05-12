b24af0ecc2889c3f429c371cbd8ca43bb6e4d239
Tareq <razzbabut@gmail.com>
2024-05-12 12:42:48 +0600

    implement update method for update queries in mongodb

--- index.js
+++ index.js
@@ -15,58 +15,83 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    const queryCollections = client.db('alternativeProducts').collection('queries');
+    try {
+        const queryCollections = client.db('alternativeProducts').collection('queries');
 
-    app.get('/queries', async(req, res)=>{
-        // console.log(req.query.userEmail)
-        let query = {};
+        app.get('/queries', async (req, res) => {
+            // console.log(req.query.userEmail)
+            let query = {};
             if (req.query?.userEmail) {
                 query = { userEmail: req.query.userEmail }
             }
             const result = await queryCollections.find(query).toArray();
-            res.send(result) 
-    })
+            res.send(result)
+        })
 
-    app.post('/queries', async(req,res)=>{
-        const queries = req.body;
-        console.log(queries);
-        const result = await queryCollections.insertOne(queries);
-        res.send(result)
-    })
+        app.get('/queries/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await queryCollections.findOne(query);
+            res.send(result)
+        })
 
-    app.delete('/deleteQuery/:id', async(req,res)=>{
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)};
-        const result = await queryCollections.deleteOne(query);
-        res.send(result)
-    })
+        app.post('/queries', async (req, res) => {
+            const queries = req.body;
+            console.log(queries);
+            const result = await queryCollections.insertOne(queries);
+            res.send(result)
+        })
 
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        app.put('/queries/:id', async (req, res) => {
+            const id = req.params.id;
+            const singleQuery = req.body;
+            const filter = { _id: new ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    product_name : singleQuery.product_name,
+                    brand_name : singleQuery.brand_name,
+                    product_image : singleQuery.product_image,
+                    query_title : singleQuery.query_title,
+                    boycotting_reason : singleQuery.boycotting_reason
+                }
+            }
+            const result = await queryCollections.updateOne(filter, updateDoc, options);
+            res.send(result)
+        })
+
+        app.delete('/deleteQuery/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await queryCollections.deleteOne(query);
+            res.send(result)
+        })
+
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
     res.send('Alternative Product Server is running')
-  })
-  
-  app.listen(port, () => {
+})
+
+app.listen(port, () => {
     console.log(`alternative-product listening on port ${port}`)
-  })
+})
